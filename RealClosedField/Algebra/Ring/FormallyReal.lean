29f85aca8c3127c5cf83758640231866eef8b218
artie2000 <artem.khovanov@gmail.com>
2025-08-20 05:47:54 +0100

    new definition for formallyreal

--- RealClosedField/Algebra/Ring/FormallyReal.lean
+++ RealClosedField/Algebra/Ring/FormallyReal.lean
@@ -5,42 +5,67 @@ Authors: Artie Khovanov
 -/
 import Mathlib.Algebra.Order.Ring.Cone
 import Mathlib.Algebra.Ring.Semireal.Defs
+import Mathlib
 
-/- TODO : decide on definition / sum stuff -/
+variable {R : Type*}
 
-class IsFormallyReal (R : Type*) [AddCommMonoid R] [Mul R] : Prop where
-  eq_zero_of_mul_self_add {a s : R} (hs : IsSumSq s) (h : a * a + s = 0) : a = 0
+variable (R) in
+class IsFormallyReal [AddCommMonoid R] [Mul R] : Prop where
+  eq_zero_of_sum_eq_zero : ∀ {s : Multiset R}, (s.map (fun a ↦ a * a)).sum = 0 → ∀ a ∈ s, a = 0
 
-theorem IsFormallyReal.eq_zero_of_mul_self {R : Type*}
-    [AddCommMonoid R] [Mul R] [IsFormallyReal R] {a : R} (h : a * a = 0) : a = 0 :=
-  IsFormallyReal.eq_zero_of_mul_self_add IsSumSq.zero (show a * a + 0 = 0 by simpa)
+theorem IsFormallyReal.eq_zero_of_mul_self [AddCommMonoid R] [Mul R] [IsFormallyReal R]
+    {a : R} : a * a = 0 → a = 0 := by simpa using IsFormallyReal.eq_zero_of_sum_eq_zero (s := {a})
 
-theorem IsFormallyReal.eq_zero_of_add_left {R : Type*}
-    [NonUnitalNonAssocSemiring R] [IsFormallyReal R] {s₁ s₂ : R}
-    (hs₁ : IsSumSq s₁) (hs₂ : IsSumSq s₂) (h : s₁ + s₂ = 0) : s₁ = 0 := by
-  induction hs₁ generalizing s₂
-  case zero            => simp_all
-  case sq_add a s hs h =>
-    have := eq_zero_of_mul_self_add (show IsSumSq (s + s₂) by aesop) (by simpa [← add_assoc])
-    aesop
+theorem AddSubmonoid.closure_eq_image_multiset_sum {M : Type*} [AddCommMonoid M] (s : Set M) :
+    ↑(closure s) = Multiset.sum '' {m : Multiset M | ∀ x ∈ m, x ∈ s} := by
+  ext x
+  refine ⟨fun h ↦ AddSubmonoid.exists_multiset_of_mem_closure h, fun h ↦ ?_⟩
+  rcases h with ⟨m, hm, rfl⟩
+  exact AddSubmonoid.multiset_sum_mem _ _ (by aesop)
 
-theorem IsFormallyReal.eq_zero_of_add_right {R : Type*}
-    [NonUnitalNonAssocSemiring R] [IsFormallyReal R] {s₁ s₂ : R}
-    (hs₁ : IsSumSq s₁) (hs₂ : IsSumSq s₂) (h : s₁ + s₂ = 0): s₂ = 0 := by
-  simp_all [eq_zero_of_add_left hs₁ hs₂ h]
+theorem isSumSq_iff_mem_range_linearCombination [AddCommMonoid R] [Mul R] {x} :
+    IsSumSq x ↔
+    ∃ s : Multiset R, (s.map (fun a ↦ a * a)).sum = x := by
+  rw [← AddSubmonoid.mem_sumSq, ← AddSubmonoid.closure_isSquare, ← SetLike.mem_coe,
+      AddSubmonoid.closure_eq_image_multiset_sum, Set.mem_image]
+  refine ⟨fun h => ?_, fun ⟨m, hm⟩ => ⟨Multiset.map (fun a ↦ a * a) m, by simp [hm]⟩⟩
+  rcases h with ⟨m, hm, rfl⟩
+  choose! sqrt hsqrt using hm
+  have hsqrt' : ∀ x ∈ m, sqrt x * sqrt x = x := by grind only
+  exact ⟨m.map sqrt, by simp [Multiset.map_congr _ hsqrt']⟩
 
-theorem isFormallyReal_of_eq_zero_of_mul_self_of_eq_zero_of_add
-    (R : Type*) [AddCommMonoid R] [Mul R]
+theorem IsFormallyReal.eq_zero_of_add_right [NonUnitalNonAssocSemiring R] [IsFormallyReal R]
+    {s₁ s₂ : R} (hs₁ : IsSumSq s₁) (hs₂ : IsSumSq s₂) (h : s₁ + s₂ = 0) : s₁ = 0 := by
+  simp_rw [isSumSq_iff_mem_range_linearCombination] at *
+  rcases hs₁ with ⟨m₁, rfl⟩
+  rcases hs₂ with ⟨m₂, rfl⟩
+  rw [← Multiset.sum_add, ← Multiset.map_add] at h
+  exact Multiset.sum_eq_zero <|by
+    simpa using fun _ _ ↦ by simp [eq_zero_of_sum_eq_zero h _ (by aesop)]
+
+theorem IsFormallyReal.eq_zero_of_add_left [NonUnitalNonAssocSemiring R] [IsFormallyReal R]
+    {s₁ s₂ : R} (hs₁ : IsSumSq s₁) (hs₂ : IsSumSq s₂) (h : s₁ + s₂ = 0): s₂ = 0 := by
+  simp_all [eq_zero_of_add_right hs₁ hs₂ h]
+
+variable (R) in
+theorem isFormallyReal_of_eq_zero_of_mul_self_of_eq_zero_of_add [AddCommMonoid R] [Mul R]
     (hz : ∀ {a : R}, a * a = 0 → a = 0)
-    (ha : ∀ {S₁ S₂ : R}, IsSumSq S₁ → IsSumSq S₂ → S₁ + S₂ = 0 → S₁ = 0) : IsFormallyReal R where
-  eq_zero_of_mul_self_add hs h := hz <| ha (IsSumSq.mul_self _) hs h
+    (ha : ∀ {s₁ s₂ : R}, IsSumSq s₁ → IsSumSq s₂ → s₁ + s₂ = 0 → s₁ = 0) : IsFormallyReal R where
+  eq_zero_of_sum_eq_zero {s} := by
+    induction s using Multiset.induction with
+    | empty => simp
+    | cons a s hs =>
+        simp only [Multiset.map_cons, Multiset.sum_cons, Multiset.mem_cons, forall_eq_or_imp]
+        intro h
+        have := ha (by simp) (by rw [isSumSq_iff_mem_range_linearCombination]; simp) h
+        exact ⟨hz this, hs (by simpa [this] using h)⟩
 
-instance {R : Type*} [NonAssocSemiring R] [Nontrivial R] [IsFormallyReal R] : IsSemireal R where
+instance [NonAssocSemiring R] [Nontrivial R] [IsFormallyReal R] : IsSemireal R where
   one_add_ne_zero hs h_contr := by
-    simpa using IsFormallyReal.eq_zero_of_add_left (by aesop) hs h_contr
+    simpa using IsFormallyReal.eq_zero_of_add_right IsSumSq.one hs h_contr
 
-instance {R : Type*} [Ring R] [LinearOrder R] [IsStrictOrderedRing R] : IsFormallyReal R where
-  eq_zero_of_mul_self_add {a} {s} hs h := mul_self_eq_zero.mp <| le_antisymm
+instance [Ring R] [LinearOrder R] [IsStrictOrderedRing R] : IsFormallyReal R where
+  eq_zero_of_sum_eq_zero {a} {s} hs h := mul_self_eq_zero.mp <| le_antisymm
     (by simpa [eq_neg_of_add_eq_zero_left h] using IsSumSq.nonneg hs) (mul_self_nonneg a)
 
 namespace RingCone
@@ -54,7 +79,7 @@ is the cone of sums of squares in `R`.
 def sumSq : RingCone T where
   __ := Subsemiring.sumSq T
   eq_zero_of_mem_of_neg_mem' {x} hx hnx :=
-    IsFormallyReal.eq_zero_of_add_left (by simpa using hx) (by simpa using hnx) (add_neg_cancel x)
+    IsFormallyReal.eq_zero_of_add_left (by simpa using hnx) (by simpa using hx) (neg_add_cancel x)
 
 @[simp] theorem sumSq_toSubsemiring : (sumSq T).toSubsemiring = .sumSq T := rfl
 
@@ -65,3 +90,5 @@ def sumSq : RingCone T where
   show Subsemiring.sumSq T = {x : T | IsSumSq x} by simp
 
 end RingCone
+
+#min_imports
