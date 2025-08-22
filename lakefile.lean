8788dcb5d1c5fe45682d2b3c03bccbef69cc440e
artie2000 <artem.khovanov@gmail.com>
2025-08-22 04:32:44 +0100

    update manifest

--- lakefile.lean
+++ lakefile.lean
@@ -16,6 +16,4 @@ lean_lib «RealClosedField» where
 
 require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
 
-meta if get_config? env = some "dev" then
-require «doc-gen4» from git
-  "https://github.com/leanprover/doc-gen4" @ "main"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
