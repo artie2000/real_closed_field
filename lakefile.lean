6fa09f362d6d8cf029509c88b973ddad3e9120cf
FM22 <artem.khovanov@gmail.com>
2025-12-29 14:24:11 +0000

    update?

--- lakefile.lean
+++ lakefile.lean
@@ -17,3 +17,9 @@ lean_lib «RealClosedField» where
 meta if get_config? env = some "dev" then
 require «doc-gen4» from git
   "https://github.com/leanprover/doc-gen4" @ "main"
+
+require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
+
+meta if get_config? env = some "dev" then
+require «doc-gen4» from git
+  "https://github.com/leanprover/doc-gen4" @ "main"
