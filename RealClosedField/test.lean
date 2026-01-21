1d56c055ec3c8964b4183ae28eb72f4bcc6e33f3
artie2000 <artem.khovanov@gmail.com>
2026-01-21 02:40:18 +0000

    test

--- RealClosedField/test.lean
+++ RealClosedField/test.lean
@@ -1,22 +1,18 @@
-def p1 : Prop := sorry
+import Mathlib
 
-def p2 : Prop := sorry
+set_option trace.aesop true in
+theorem test {R : Type*} [AddMonoid R] [Mul R]
+    {a s : R} (ha : a ≠ 0)
+    (ih : s ∈ AddSubsemigroup.closure {x | ∃ y, y ≠ 0 ∧ y * y = x}) :
+    a * a + s ∈ AddSubsemigroup.closure {x | ∃ y, y ≠ 0 ∧ y * y = x} := by
+  aesop
 
-theorem imp : p2 → p1 := sorry
-
-def data (h : p1) := 0
-
-@[simp]
-theorem fact (h : p2) : data (imp h) = 0 := rfl
-
-set_option pp.proofs true in
-set_option trace.Meta.isDefEq true in
-example (h : p2) : data (imp h) = 0 := by simp only [fact]
-/-
-[Meta.Tactic.simp.discharge] fact discharge ❌️
-      p2
-[Meta.Tactic.simp.unify] eq_self.{u_1}:1000, failed to unify
-      @Eq.{?u.116} ?α ?a ?a
-    with
-      @Eq.{1} Nat (data (imp h)) (@OfNat.ofNat.{0} Nat (nat_lit 0) (instOfNatNat (nat_lit 0)))
--/
+set_option trace.aesop true in
+theorem works {R : Type*} [AddMonoid R] [Mul R]
+    {a s : R} (ha : a ≠ 0)
+    (ih : s ∈ AddSubsemigroup.closure {x | ∃ y, y ≠ 0 ∧ y * y = x}) :
+    a * a + s ∈ AddSubsemigroup.closure {x | ∃ y, y ≠ 0 ∧ y * y = x} := by
+  apply add_mem -- aesop safe apply 90%
+  apply AddSubsemigroup.mem_closure_of_mem -- aesop safe apply 90%
+  · aesop
+  · aesop
