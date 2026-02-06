bd40a4cd49003596e0c3d644fbfa9c74c6447286
artie2000 <artem.khovanov@gmail.com>
2026-02-06 01:30:59 +0000

    sync test

--- RealClosedField/test.lean
+++ RealClosedField/test.lean
@@ -16,3 +16,13 @@ theorem works {R : Type*} [AddMonoid R] [Mul R]
   apply AddSubsemigroup.mem_closure_of_mem -- aesop safe apply 90%
   · aesop
   · aesop
+
+variable {K : Type*} [Field K] [IsAlgClosed K]
+-- variable {R : Type*} [CommRing R] [IsDomain R] [Algebra K R]
+variable {L : Type*} [Field L] [Algebra K L]
+
+open IntermediateField in
+example {x : L} (hx : IsAlgebraic K x) : ∃ y, algebraMap K L y = x := by
+  have := isAlgebraic_adjoin_simple hx.isIntegral
+  simpa [adjoin_eq_bot_iff, IntermediateField.mem_bot] using
+    eq_bot_of_isAlgClosed_of_isAlgebraic K⟮x⟯
